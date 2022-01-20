8883b0ca66ac3359a125d5dc4ae086cd7c78840d
Iñigo Imizcoz <70577097+inimicoz@users.noreply.github.com>
2022-01-20 09:38:52 +0100

    Create Main.js

---
+++ Covid/Main.js
@@ -0,0 +1,44 @@
+var getPromisify = (url, data, dataType) => {
+  return new Promise((resolve, reject) => {
+    $.post(
+      url,
+      data,
+      (response, status, xhr) => {
+        if (status === "success") {
+          resolve({ response, status, xhr });
+        } else {
+          const err = new Error("xhr error");
+          err.target = xhr;
+          reject(err);
+          console.log("xhr error");
+        }
+      },
+      dataType
+    );
+  });
+};
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+    // ------------------
+    // Scripting methods
+    // ------------------
+    async get(url, data, dataType) {
+      const r = await getPromisify(url, data, dataType);
+      return [
+        r.response.positiveCasesViral,
+        r.response.positiveIncrease,
+        r.response.hospitalizedCurrently,
+      ];
+    }
+  }
+
+  customElements.define("dk-test-covid", MainWebComponent);
+})();
